ba1ec38bc0783abf038e222fb62c1c8564befc14
Sanjeev Chauhan <isitsanjeev@gmail.com>
2020-10-28 20:04:23 +0530

    Product n User Update

--- Models/products.js
+++ Models/products.js
@@ -3,6 +3,12 @@ var db = require('../dbConnection'); //reference of dbconnection.js file
 var Product = {
     getProductDet: function (callback) {
         db.query('select * from product_list', callback);
+    },
+    addProduct: function (product, callback) {
+        return db.query('insert into product_list values(?,?,?,?)', [product.id, product.product_name, product.product_desc, product.product_price], callback);
+    },
+    deleteProduct: function (id, callback) {
+        return db.query('delete from product_list where id=?', [id], callback);
     }
 }
 
--- routes/product_route.js
+++ routes/product_route.js
@@ -3,13 +3,36 @@ var router = express.Router();
 var prod = require('../Models/products');
 
 router.get('/', function (req, res, next) {
-    prod.getProductDet(function (req, res) {
+    prod.getProductDet(function (err, rows) {
         if (err) {
             res.json(err);
         } else {
             res.json(rows);
         }
     });
+});
+router.post('/', function (req, res, next) {
+
+    prod.addProduct(req.body, function (err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(rows);
+        }
+
+    });
+});
+
+router.delete('/:id', function (req, res, next) {
+
+    prod.deleteProduct(req.params.id, function (err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(rows);
+        }
+    });
+
 });
 
 module.exports = router;
